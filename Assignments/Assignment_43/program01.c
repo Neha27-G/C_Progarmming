25cd1847930f49175489155bafaad5caac0d8d85
Neha27-G <niagupta8@gmail.com>
2026-07-26 23:50:28 +0530

    Program which recursively display given pattern

---
+++ Assignments/Assignment_43/program01.c
@@ -0,0 +1,26 @@
+//Write a recursive program which display below pattern
+
+//output :      *      *      *      *       *
+
+#include<stdio.h>
+
+void Display()
+{
+    static int i = 1;
+
+    if(i<= 5)
+    { 
+        printf("*\t");
+        i++;
+
+        Display();
+    }
+}
+
+int main()
+{
+    Display();
+    printf("\n\n");
+
+    return 0;
+}
