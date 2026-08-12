1a8f90ed41cfd80820bcd59b454a98aae828115c
Neha27-G <niagupta8@gmail.com>
2026-08-12 17:01:11 +0530

    Program of Linear Search  using Array

---
+++ Application Programs/program103.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+bool LinearSearch(int Arr[], int iSize, int iNo)
+{
+    bool bFlag = false;
+    int iCnt = 0;
+
+    for(iCnt = 0 ; iCnt < iSize; iCnt++)
+    {
+        if(Arr[iCnt] == iNo)
+        {
+            bFlag = true;
+            break;
+        }
+    }
+    return bFlag;
+}
+
+int main()
+{
+    int *Brr = NULL;
+    int iLength = 0, iCnt = 0, iValue = 0;
+    bool bRet = false;
+
+    printf("Enter the Number of Elements :\n");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(sizeof(int) * iLength);
+
+    printf("Enter the Elements :\n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    printf("Enter the Element that you want to search :\n");
+    scanf("%d",&iValue);
+
+    bRet = LinearSearch(Brr, iLength, iValue);
+
+    if(bRet == true)
+    {
+        printf("Elements is present\n");
+    }
+    else
+    {
+        printf("Elements is not present\n");
+    }
+
+    free(Brr);
+
+    return 0;
+
+}
